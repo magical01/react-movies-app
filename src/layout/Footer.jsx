3169d1a5fd7de2f8d4331a14278f7a700252a024
magical01 <denis.verzakov.015@gmail.com>
2022-11-06 18:20:26 +0500

    feat: add link

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer() {
   return <footer className="bg-sky-200 h-20 shadow-xxl">
     <div className="container mx-auto flex justify-between px-5 items-center h-full">
       <span className="font-sans text-lg text-white">Text Copyright {new Date().getFullYear()}</span>
-      <a className="footer__link font-sans text-lg text-white" href="#">Repository</a>
+      <a className="footer__link font-sans text-lg text-white" href="https://github.com/magical01/react-movies-app" target={"_blank"}>Repository</a>
     </div>
   </footer>
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return <header className="header bg-sky-600 h-20 shadow-xl">
     <div className="container mx-auto flex justify-between px-5 items-center h-full">
       <a className="header__link font-sans text-3xl text-white" href="#">React Movies App</a>
-      <a className="header__link font-sans text-lg text-white" href="#">Repository</a>
+      <a className="header__link font-sans text-lg text-white" href="https://github.com/magical01/react-movies-app" target={"_blank"}>Repository</a>
     </div>
   </header>
 }
